1571d663c2df357e2ee2a358d5f221e5fa699c49
21200346 <21200346@ens.uvsq.fr>
2015-11-26 21:24:23 +0100

    Add ex3_6

---
+++ exercices/src/main/java/fr/uvsq/coo/ex3_6/Printer.java
@@ -0,0 +1,6 @@
+package fr.uvsq.coo.ex3_6;
+
+public interface Printer
+{
+	void print();
+}
---
+++ exercices/src/main/java/fr/uvsq/coo/ex3_6/SimplePrinter.java
@@ -0,0 +1,9 @@
+package fr.uvsq.coo.ex3_6;
+
+public class SimplePrinter implements Printer
+{
+	public void print()
+	{
+		// Print a document
+	}
+}
